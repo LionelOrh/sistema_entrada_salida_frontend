64064f83d5d4ec134d59203caefb04eb5ce85f0e
jarenSg <jarencesar127@gmail.com>
2024-04-27 01:45:46 -0500

    cambios

--- src/app/services/sala.service.ts
+++ src/app/services/sala.service.ts
@@ -1,3 +1,6 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Sala } from '../models/sala.model';
 import { Injectable } from '@angular/core';
 import { AppSettings } from '../app.settings';
 
@@ -8,5 +11,9 @@ const baseUrlSala = AppSettings.API_ENDPOINT+ '/sala';
 })
 export class SalaService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  
+  registrar(data:Sala):Observable<any>{
+    return this.http.post(baseUrlSala, data);
+  }
 }
