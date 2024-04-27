150a686ea7d0aa519f4580ad09ef8b0e69b6d32a
7ionel <lionelorihuelac@gmail.com>
2024-04-27 12:59:48 -0500

    registro Alumno-Finalizado

--- src/app/components/agregar-alumno/agregar-alumno.component.ts
+++ src/app/components/agregar-alumno/agregar-alumno.component.ts
@@ -1,21 +1,105 @@
 import { Component, OnInit } from '@angular/core';
 import { AppMaterialModule } from '../../app.material.module';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../menu/menu.component';
+import { Pais } from '../../models/pais.model';
+import { DataCatalogo } from '../../models/dataCatalogo.model';
+import { UtilService } from '../../services/util.service';
+import { Usuario } from '../../models/usuario.model';
+import { AlumnoService } from '../../services/alumno.service';
+import { TokenService } from '../../security/token.service';
+import { Alumno } from '../../models/alumno.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-alumno',
   standalone: true,
-  imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent],
+  imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent,ReactiveFormsModule],
   templateUrl: './agregar-alumno.component.html',
   styleUrls: ['./agregar-alumno.component.css']
 })
-export class AgregarAlumnoComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+export class AgregarAlumnoComponent{
+
+  ltsPais: Pais[]=[]
+  ltsModalidad: DataCatalogo[]=[]
+
+  objAlumno: Alumno={
+    nombres:"",
+    apellidos:"",
+    telefono:"",
+    celular:"",
+    dni:"",
+    correo:"",
+    tipoSangre:"",
+    fechaNacimiento : undefined,
+    pais:{
+      idPais:-1
+    },
+    modalidad:{
+      idDataCatalogo : -1
+    }
   }
 
+
+  objUsuario : Usuario={};
+
+  formsRegistra = this.FormBuilder.group({ 
+        validaNombre: ['', [Validators.required, Validators.pattern('[a-zA-Zá-úÁ-ÚñÑ ]{3,30}')]] , 
+        validaApellido: ['', [Validators.required, Validators.pattern('[a-zA-Zá-úÁ-ÚñÑ ]{3,30}')]] , 
+        validaTelefono: ['', [Validators.required, Validators.pattern('[0-9]{9}')] ] , 
+        validaCelular: ['', [Validators.required, Validators.pattern('[0-9]{9}')] ] ,
+        validaDni: ['', [Validators.required, Validators.pattern('[0-8]{8}')] ] ,  
+        validaCorreo: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')]] ,
+        validaTipoSangre: ['', [Validators.required, Validators.pattern('^[ABO][+-]?$|^(A|B|AB|O)(\\+|-)$-----------------')]],
+        validaFecha: ['', [Validators.required] ] , 
+         validaPais: ['', Validators.min(1)] , 
+        validaModalidad: ['', Validators.min(1)] , 
+      });
+
+  constructor(private utilService: UtilService, 
+    private alumnoService:AlumnoService,
+     private tokenService:TokenService, 
+     private FormBuilder: FormBuilder ) {
+      
+    this.utilService.listaPais().subscribe(
+      x=> this.ltsPais=x
+    );
+    this.utilService.listaModalidadAlumno().subscribe(
+      x=> this.ltsModalidad= x
+    );
+    this.objUsuario.idUsuario = this.tokenService.getUserId();
+   }
+
+   registra(){
+    if (this.formsRegistra.valid){
+    this.objAlumno.usuarioActualiza=this.objUsuario;
+    this.objAlumno.usuarioRegistro = this.objUsuario;
+
+    this.alumnoService.registra(this.objAlumno).subscribe(
+        x =>
+          Swal.fire({
+            icon: 'success',
+            title: 'Resultado del Registro',
+            text: x.mensaje,
+          })
+    );
+    this.objAlumno = {
+      nombres: "",
+      apellidos: "",
+      telefono: "",
+      celular: "",
+      dni: "",
+      correo: "",
+      tipoSangre: "",
+      fechaNacimiento: undefined,
+      pais: { idPais: -1 },
+      modalidad: { idDataCatalogo: -1 }
+    };
+    this.formsRegistra.reset();
+    
+}
+}
 }
--- src/app/models/alumno.model.ts
+++ src/app/models/alumno.model.ts
@@ -1,7 +1,19 @@
+import { DataCatalogo } from "./dataCatalogo.model";
+import { Pais } from "./pais.model";
+import { Usuario } from "./usuario.model";
 
 export class Alumno {
-
-
-
-   
+    idAlumno?:number;
+    nombres?: string;
+    apellidos?: string;
+    telefono?:string;
+    celular?:string;
+    dni?: string;
+    correo?: string;
+    tipoSangre?: string;
+    fechaNacimiento?: Date;
+    pais?: Pais;
+    modalidad?: DataCatalogo;
+    usuarioRegistro?: Usuario;
+    usuarioActualiza?: Usuario
 }
