2f1b22bd9ba1b9477a4823551a649da72ffd9524
Jans110718 <jansyaredjade111@gmail.com>
2024-04-27 13:50:39 -0500

    finalizadoConValidaciones

--- src/app/components/agregar-editorial/agregar-editorial.component.ts
+++ src/app/components/agregar-editorial/agregar-editorial.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppMaterialModule } from '../../app.material.module';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../menu/menu.component';
 import { Pais } from '../../models/pais.model';
@@ -10,50 +10,81 @@ import { Editorial } from '../../models/editorial.model';
 import { TokenService } from '../../security/token.service';
 import { Usuario } from '../../models/usuario.model';
 import { EditorialService } from '../../services/editorial.service';
-import Swal from 'sweetalert2';
+
+import Swal from 'sweetalert2'
+import { BrowserModule } from '@angular/platform-browser';
+
 @Component({
   standalone: true,
-  imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent],
+  imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent, ReactiveFormsModule],
   selector: 'app-agregar-editorial',
   templateUrl: './agregar-editorial.component.html',
   styleUrls: ['./agregar-editorial.component.css']
 })
 export class AgregarEditorialComponent {
 
-  lstPais: Pais[] = [];
-  objeditorial: Editorial ={
-        razonSocial: "",
-        direccion: "",
-        ruc:"",
-        gerente: "",
-        fechaCreacion : new Date(),
-        pais:{
-          idPais:-1
+      lstPais: Pais[] = [];
+
+      objEditorial: Editorial ={
+            razonSocial: "",
+            direccion: "",
+            ruc:"",
+            gerente:"",
+            fechaCreacion : undefined,
+            pais:{
+              idPais:-1
+            }
         }
-    }
-    objUsuario: Usuario = {} ;
+        objUsuario: Usuario = {} ;
 
-  constructor(private utilService: UtilService, 
-              private tokenService: TokenService,
-              private editorialService: EditorialService){
-          this.utilService.listaPais().subscribe(
-            x =>  this.lstPais = x
-          );
-          this.objUsuario.idUsuario = tokenService.getUserId();
-  }
-  registra(){
-        this.objeditorial.usuarioActualiza = this.objUsuario;
-        this.objeditorial.usuarioRegistro = this.objUsuario;
-        this.editorialService.registrar(this.objeditorial).subscribe(
-          x=>{
-            Swal.fire({
-              icon: 'info',
-              title: 'Resultado del Registro',
-              text: x.mensaje,
-            })
-          },
-        );
-  }
+      formsRegistra = this.formBuilder.group({  
+     validarazonSocial: ['', [Validators.required, Validators.pattern('[a-zA-Zá-úÁ-ÚñÑ ]{3,100}')]],
+    validadireccion: ['', [Validators.required, Validators.maxLength(100)]],
+    validaruc: ['', [Validators.required, Validators.pattern('[0-9]{11}')]], 
+    validagerente: ['', [Validators.required, Validators.maxLength(50)]],
+    validafechaCreacion: ['', [Validators.required] ] , 
+    validaPais: ['', Validators.min(1)],
+      });
+
+      constructor(private utilService: UtilService, 
+                  private tokenService: TokenService,
+                  private  editorialService: EditorialService,
+                  private formBuilder: FormBuilder){
+              
+              this.utilService.listaPais().subscribe(
+                x =>  this.lstPais = x
+              );
+              this.objUsuario.idUsuario = tokenService.getUserId();
+      }
+      registra(){
+        if (this.formsRegistra.valid){
+
+            this.objEditorial.usuarioActualiza = this.objUsuario;
+            this.objEditorial.usuarioRegistro = this.objUsuario;
+            this.editorialService.registrar(this.objEditorial).subscribe(
+              x=>{
+                Swal.fire({
+                  icon: 'info',
+                  title: 'Resultado del Registro',
+                  text: x.mensaje,
+                });        
+         this.formsRegistra.reset();
+         
+         this.objEditorial = {
+           razonSocial: "",
+           direccion: "",
+           ruc: "",
+           gerente: "",
+           fechaCreacion: undefined,
+           pais: {
+             idPais: -1
+           }
+         };
+       },
+       error => {
+         console.error('Error al registrar:', error);
+       }
+     );
+   }
+ }
 }
-
-
