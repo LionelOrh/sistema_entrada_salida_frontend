20a6705996b5960bca66b21538279f556325e011
18luzD <Luz18cabanillaspizarro@gmail.com>
2024-04-26 21:49:32 -0500

    cambios

--- src/app/components/agregar-proveedor/agregar-proveedor.component.ts
+++ src/app/components/agregar-proveedor/agregar-proveedor.component.ts
@@ -3,19 +3,65 @@ import { AppMaterialModule } from '../../app.material.module';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../menu/menu.component';
-
+import { Pais } from '../../models/pais.model';
+import { DataCatalogo } from '../../models/dataCatalogo.model';
+import { UtilService } from '../../services/util.service';
+import { Proveedor } from '../../models/proveedor.model';
+import { TokenService } from '../../security/token.service';
+import { Usuario } from '../../models/usuario.model';
+import { ProveedorService } from '../../services/proveedor.service';
+import Swal from 'sweetalert2';
 @Component({
   standalone: true,
   imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent],
-  selector: 'app-agregar-proveedor',
-  templateUrl: './agregar-proveedor.component.html',
-  styleUrls: ['./agregar-proveedor.component.css']
+  selector: 'app-agregar-Proveedor',
+  templateUrl: './agregar-Proveedor.component.html',
+  styleUrls: ['./agregar-Proveedor.component.css']
 })
-export class AgregarProveedorComponent implements OnInit {
+export class AgregarProveedorComponent {
 
-  constructor() { }
+  lstPais: Pais[] = [];
+  lstTipo: DataCatalogo[] = []; 
+  objProveedor: Proveedor ={
+        razonSocial: "",
+        ruc:"",
+        direccion: "",
+        telefono:"",
+        celular:"",
+        contacto:"",
+        fechaRegistro : new Date(),
+        pais:{
+          idPais:-1
+        },
+        tipoProveedor:{
+          idDataCatalogo:-1
+      }
+    }
+    objUsuario: Usuario = {} ;
 
-  ngOnInit(): void {
+  constructor(private utilService: UtilService, 
+              private tokenService: TokenService,
+              private ProveedorService: ProveedorService){
+                this.utilService.listaTipoProveedor().subscribe(
+                  x =>  this.lstTipo = x
+            );
+          this.utilService.listaPais().subscribe(
+            x =>  this.lstPais = x
+          );
+          this.objUsuario.idUsuario = tokenService.getUserId();
   }
-
+  registra(){
+        this.objProveedor.usuarioActualiza = this.objUsuario;
+        this.objProveedor.usuarioRegistro = this.objUsuario;
+        this.ProveedorService.registrar(this.objProveedor).subscribe(
+          x=>{
+            Swal.fire({
+              icon: 'info',
+              title: 'Resultado del Registro',
+              text: x.mensaje,
+            })
+          },
+        );
+  }
 }
+
--- src/app/models/proveedor.model.ts
+++ src/app/models/proveedor.model.ts
@@ -1,2 +1,21 @@
+import { DataCatalogo } from "./dataCatalogo.model";
+import { Usuario } from "./usuario.model";
+import { Pais } from "./pais.model";
+
 export class Proveedor {
+
+
+    idEditorial?: number;
+    razonSocial?:string;
+    ruc?:string;
+    direccion?:string;
+    telefono?:string;
+    celular?:string;
+    contacto?:string;
+    estado?: number;
+    fechaRegistro?:Date;
+    pais?:Pais;
+    tipoProveedor?:DataCatalogo;
+    usuarioRegistro?:Usuario;
+    usuarioActualiza?:Usuario;
 }
