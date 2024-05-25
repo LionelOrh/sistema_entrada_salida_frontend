92a3a4ee987bad93fe37c942baee39096716ea11
Jans110718 <jansyaredjade111@gmail.com>
2024-05-25 11:27:36 -0500

    Validaciones-Registra

--- src/app/components/agregar-editorial/agregar-editorial.component.ts
+++ src/app/components/agregar-editorial/agregar-editorial.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppMaterialModule } from '../../app.material.module';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../menu/menu.component';
 import { Pais } from '../../models/pais.model';
@@ -13,7 +13,51 @@ import { EditorialService } from '../../services/editorial.service';
 
 import Swal from 'sweetalert2'
 import { BrowserModule } from '@angular/platform-browser';
+import { Observable, of } from 'rxjs';
+import { map, catchError, debounceTime, switchMap } from 'rxjs/operators';
+// Validador personalizado para comprobar si el RUC comienza con 10
+function rucStartsWith10(control: AbstractControl): ValidationErrors | null {
+  const ruc = control.value;
+  if (ruc && !ruc.startsWith('10')) {
+    return { startsWith10: true };
+  }
+  return null;
+}
+// Validador  para verificar si el RUC es único
+function rucUnique(editorialService: EditorialService) {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    if (!control.value) {
+      return of(null);
+    }
+    return editorialService.validaRucActualiza(control.value).pipe(
+      debounceTime(500),
+      map(response => (response.valid ? null : { rucNotUnique: true })),
+      catchError(() => of(null))
+    );
+  };
+}
+// Validador  para verificar si la razon social es única
+function razonSocialUnique(editorialService: EditorialService) {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    if (!control.value) {
+      return of(null);
+    }
+    return editorialService.validaRazonSocial(control.value).pipe(
+      debounceTime(500),
+      map(response => (response.valid ? null : { razonSocialNotUnique: true })),
+      catchError(() => of(null))
+    );
+  };
+}
 
+// Validador personalizado para comprobar si la fecha es de 1980 en adelante
+function dateFrom1980Onwards(control: AbstractControl): ValidationErrors | null {
+  const date = new Date(control.value);
+  if (date.getFullYear() < 1980) {
+    return { dateFrom1980Onwards: true };
+  }
+  return null;
+}
 @Component({
   standalone: true,
   imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent, ReactiveFormsModule],
@@ -38,12 +82,12 @@ export class AgregarEditorialComponent {
         objUsuario: Usuario = {} ;
 
       formsRegistra = this.formBuilder.group({  
-     validarazonSocial: ['', [Validators.required, Validators.pattern('[a-zA-Zá-úÁ-ÚñÑ ]{3,100}')]],
-    validadireccion: ['', [Validators.required, Validators.maxLength(100)]],
-    validaruc: ['', [Validators.required, Validators.pattern('[0-9]{11}')]], 
-    validagerente: ['', [Validators.required, Validators.maxLength(50)]],
-    validafechaCreacion: ['', [Validators.required] ] , 
-    validaPais: ['', Validators.min(1)],
+        validarazonSocial: ['', [Validators.required, Validators.pattern('[a-zA-Zá-úÁ-ÚñÑ ]{4,40}')], [razonSocialUnique(this.editorialService)]],
+        validadireccion: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(40)]],
+        validaruc: ['', [Validators.required, Validators.pattern('[0-9]{11}'), rucStartsWith10], [rucUnique(this.editorialService)]],
+        validagerente: ['', [Validators.required, Validators.pattern('[a-zA-Zá-úÁ-ÚñÑ ]{3,50}$')]],
+        validafechaCreacion: ['', [Validators.required, dateFrom1980Onwards]],
+        validaPais: ['', [Validators.min(1)]],
       });
 
       constructor(private utilService: UtilService, 
