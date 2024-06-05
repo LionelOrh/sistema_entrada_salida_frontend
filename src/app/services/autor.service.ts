f336046c9c36d118530f6c271f19f027cc3801e4
AstridYovera <ayoveratinoco@gmail.com>
2024-06-04 21:32:44 -0500

    pc3 avance

--- src/app/services/autor.service.ts
+++ src/app/services/autor.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
 import { AppSettings } from '../app.settings';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Autor } from '../models/autor.model';
 import { Observable } from 'rxjs';
 
-const baseUrlAutor = AppSettings.API_ENDPOINT+ '/autor';
+const baseUrlAutor = AppSettings.API_ENDPOINT + '/autor';
 const baseUrlCrudAutor = AppSettings.API_ENDPOINT + '/crudAutor';
+const baseUrlConsultaAutor = AppSettings.API_ENDPOINT+ '/consultaAutor';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutorService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   //PC1 - Registrar
-  registrar(data:Autor):Observable<any>{
+  registrar(data: Autor): Observable<any> {
     return this.http.post(baseUrlAutor, data);
   }
 
@@ -32,5 +34,32 @@ export class AutorService {
   consultarCrud(filtro: string): Observable<any> {
     return this.http.get(baseUrlCrudAutor + "/listaAutorPorNombresLike/" + filtro);
   }
+  ConsultaAutorComplejo(
+    nombres: string,
+    apellidos: string,
+    fechaNacimientoDesde: string,
+    fechaNacimientoHasta: string,
+    telefono: string,
+    celular: string,
+    orcid: string,
+    estado: number,
+    idPais: number,
+    idGrado: number
+  ): Observable<any> {
+    const params = new HttpParams()
+      .set("nombres", nombres)
+      .set("apellidos", apellidos)
+      .set("fechaNacimientoDesde", fechaNacimientoDesde)
+      .set("fechaNacimientoHasta", fechaNacimientoHasta)
+      .set("telefono", telefono)
+      .set("celular", celular)
+      .set("orcid", orcid)
+      .set("estado", estado)
+      .set("idPais", idPais)
+      .set("idGrado", idGrado)
+
+    return this.http.get(baseUrlConsultaAutor + "/consultaAutorPorParametros", { params })
+  }
+
 
 }
